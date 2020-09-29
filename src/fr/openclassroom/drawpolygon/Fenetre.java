79fd710ef622a17bd4aa741e987d430bc12de681
Sebastien Fremy <sebastien.fremy1986@gmail.com>
2020-09-29 21:22:11 +0200

    Ajout des méthodes drawPolygon() et drawPolyline()

--- src/fr/openclassroom/drawpolygon/Fenetre.java
+++ src/fr/openclassroom/drawpolygon/Fenetre.java
@@ -6,7 +6,7 @@ public class Fenetre extends JFrame {
 
     public Fenetre(){
         this.setTitle("Ma première fenêtre Java");
-        this.setSize(100, 150);
+        this.setSize(150, 200);
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setContentPane(new Panneau());
--- src/fr/openclassroom/drawpolygon/Panneau.java
+++ src/fr/openclassroom/drawpolygon/Panneau.java
@@ -6,8 +6,16 @@ import java.awt.*;
 public class Panneau extends JPanel {
 
     public void paintComponent(Graphics g){
-        // x1, y1, width, height, arcWidth, arcHeight
-        g.drawLine(0, 0, this.getWidth(), this.getHeight());
-        g.drawLine(0, this.getHeight(), this.getWidth(), 0);
+        int x[] = {20, 30, 50, 60, 60, 50, 30, 20};
+        int y[] = {30, 20, 20, 30, 50, 60, 60, 50};
+        g.drawPolygon(x, y, 8);
+
+        int x2[] = {50, 60, 80, 90, 90, 80, 60, 50};
+        int y2[] = {60, 50, 50, 60, 80, 90, 90, 80};
+        g.fillPolygon(x2, y2, 8);
+
+        int x3[] = {90, 100, 110, 120, 120, 110, 100, 90};
+        int y3[] = {100, 90, 90, 100, 120, 130, 130, 120};
+        g.drawPolyline(x3, y3, 8);
     }
 }
