28065e1305ec53540f1162f554e48c365319f0ff
Sebastien Fremy <sebastien.fremy1986@gmail.com>
2020-09-29 21:51:00 +0200

    Ajout de la méthode drawImage()

---
+++ src/fr/openclassroom/drawimage/JframeDeuxExemple.java
@@ -0,0 +1,10 @@
+package fr.openclassroom.drawimage;
+
+public class JframeDeuxExemple {
+
+    public static void main(String[] args){
+
+        Fenetre fen = new Fenetre();
+
+    }
+}
---
+++ src/fr/openclassroom/drawimage/Panneau.java
@@ -0,0 +1,23 @@
+package fr.openclassroom.drawimage;
+
+import java.awt.Graphics;
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+
+public class Panneau extends JPanel {
+
+    public void paintComponent(Graphics g){
+        try {
+            Image img = ImageIO.read(new File("java.jpg"));
+            //g.drawImage(img, 0, 0, this);
+            g.drawImage(img, 0, 0, 300, 300, this);
+            // Pour une image de fond
+            // g.drawImage(img, 0, 0, this.getWidth(), this.getHeight(), this);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
