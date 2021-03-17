e5547d470b16473a25b546d067c971ae325b6feb
Sebastien Fremy <sebastien.fremy1986@gmail.com>
2021-03-17 17:37:28 +0100

    Ajout de JRadioButton

---
+++ src/fr/openclassroom/formulaire/jRadioButton/ButtonGroup/Main.java
@@ -0,0 +1,11 @@
+package fr.openclassroom.formulaire.jRadioButton.ButtonGroup;
+
+
+public class Main {
+
+    public static void main(String[] args){
+
+        Fenetre fenetre = new Fenetre();
+
+    }
+}
---
+++ src/fr/openclassroom/formulaire/jRadioButton/simple/Fenetre.java
@@ -0,0 +1,37 @@
+package fr.openclassroom.formulaire.jRadioButton.simple;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Fenetre extends JFrame {
+    private JPanel container = new JPanel();
+    private JRadioButton jr1 = new JRadioButton("Radio 1");
+    private JRadioButton jr2 = new JRadioButton("Radio 2");
+
+    public Fenetre(){
+        this.setTitle("Animation");
+        this.setSize(300, 300);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+        container.setBackground(Color.white);
+        container.setLayout(new BorderLayout());
+
+        JPanel top = new JPanel();
+        jr1.addActionListener(new StateListener());
+        jr2.addActionListener(new StateListener());
+        top.add(jr1);
+        top.add(jr2);
+        container.add(top, BorderLayout.NORTH);
+        this.setContentPane(container);
+        this.setVisible(true);
+    }
+
+    class StateListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            System.out.println("source : " + ((JRadioButton)e.getSource()).getText() + " - état : " +
+                    ((JRadioButton)e.getSource()).isSelected());
+        }
+    }
+}
